caf763175e5e4d91d7ac8543738a5cf58af9d7c7
Shubham3588 <62181875+Shubham3588@users.noreply.github.com>
2024-07-01 02:45:37 +0530

    added calculator programs

---
+++ src/simpleCalculator/mainCalculatorClass.java
@@ -0,0 +1,14 @@
+package simpleCalculator;
+
+public class mainCalculatorClass {
+
+    public static void main(String[] args) {
+        calculator ca = new calculator();
+
+        ca.setFirstNumber(5.0);
+        ca.setSecondNumber(6.7);
+
+
+        ca.getAdditionResult();
+    }
+}
